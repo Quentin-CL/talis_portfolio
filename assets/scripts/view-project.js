2e92f70984f91e2ce65e5a1e429e59fbc23e1b95
Quentin Colombier <quent.colombier@gmail.com>
2023-09-05 14:15:49 +0200

    Added a correction to buttons

--- assets/scripts/view-project.js
+++ assets/scripts/view-project.js
@@ -10,57 +10,60 @@ const links = [
 ];
 
 
-// Setup the cursor tracker on all the articles with a recursive function
-articles.forEach((article, i) => {
-    let [offsetTop, offsetLeft, mouseX, mouseY, cursorX, cursorY, rotate] = initializePosition(article);
-    function animate() {
-        (rotate != 360) ? rotate += 0.5 : rotate = 0;
-        let distX = mouseX - cursorX - offsetLeft;
-        let distY = mouseY - cursorY - offsetTop;
-        cursorX = cursorX + (distX * speed);
-        cursorY = cursorY + (distY * speed);
+window.addEventListener('load', function () {
 
-        cursors[i].style.left = cursorX + 'px';
-        cursors[i].style.top = cursorY + 'px';
-        cursors[i].style.transform = ` translate(-50%,-50%) rotate(${rotate}deg)`;
-        myReq = requestAnimationFrame(animate);
-    }
-    animate();
+    // Setup the cursor tracker on all the articles with a recursive function
+    articles.forEach((article, i) => {
+        let [offsetTop, offsetLeft, mouseX, mouseY, cursorX, cursorY, rotate] = initializePosition(article);
+        function animate() {
+            (rotate != 360) ? rotate += 0.5 : rotate = 0;
+            let distX = mouseX - cursorX - offsetLeft;
+            let distY = mouseY - cursorY - offsetTop;
+            cursorX = cursorX + (distX * speed);
+            cursorY = cursorY + (distY * speed);
 
-    // Retrieve the postion of the mouse whenever the mouse move in the article
-    article.addEventListener('mousemove', (event) => {
-        mouseX = event.pageX;
-        mouseY = event.pageY;
+            cursors[i].style.left = cursorX + 'px';
+            cursors[i].style.top = cursorY + 'px';
+            cursors[i].style.transform = ` translate(-50%,-50%) rotate(${rotate}deg)`;
+            myReq = requestAnimationFrame(animate);
+        }
+        animate();
+
+        // Retrieve the postion of the mouse whenever the mouse move in the article
+        article.addEventListener('mousemove', (event) => {
+            mouseX = event.pageX;
+            mouseY = event.pageY;
+        })
+
+        // Reset the position of the buttons when the mouse leave the article
+        article.addEventListener('mouseleave', () => {
+            res = initializePosition(article);
+            mouseX = res[2];
+            mouseY = res[3];
+        })
+
+        // Reset the positions of buttons and adjust offsets when the window is resized
+        window.addEventListener('resize', () => {
+            res = initializePosition(article);
+            [offsetTop, offsetLeft, mouseX, mouseY] = res
+        })
     })
 
-    // Reset the position of the buttons when the mouse leave the article
-    article.addEventListener('mouseleave', () => {
-        res = initializePosition(article);
-        mouseX = res[2];
-        mouseY = res[3];
-    })
+    // Initialize the position of buttons
+    function initializePosition(container) {
+        const offsetTop = container.parentElement.offsetTop;
+        const offsetLeft = container.parentElement.offsetLeft;
+        let mouseX = offsetLeft + container.parentElement.clientWidth * 0.25;
+        let mouseY = offsetTop + container.clientHeight;
+        let cursorX = container.parentElement.clientWidth * 0.25;
+        let cursorY = container.clientHeight;
+        let rotate = 0;
+        return [offsetTop, offsetLeft, mouseX, mouseY, cursorX, cursorY, rotate]
+    };
 
-    // Reset the positions of buttons and adjust offsets when the window is resized
-    window.addEventListener('resize', () => {
-        res = initializePosition(article);
-        [offsetTop, offsetLeft, mouseX, mouseY] = res
-    })
-})
-
-// Initialize the position of buttons
-function initializePosition(container) {
-    const offsetTop = container.parentElement.offsetTop;
-    const offsetLeft = container.parentElement.offsetLeft;
-    let mouseX = offsetLeft + container.parentElement.clientWidth * 0.25;
-    let mouseY = offsetTop + container.clientHeight;
-    let cursorX = container.parentElement.clientWidth * 0.25;
-    let cursorY = container.clientHeight;
-    let rotate = 0;
-    return [offsetTop, offsetLeft, mouseX, mouseY, cursorX, cursorY, rotate]
-};
-
-buttons.forEach((button, i) => {
-    button.addEventListener('click', () => {
-        window.open(links[i], "_blank")
+    buttons.forEach((button, i) => {
+        button.addEventListener('click', () => {
+            window.open(links[i], "_blank")
+        })
     })
 })
